c676f32998815e5ad523d190b457c570ee1ae172
DenSLRR <denslrrwork@gmail.com>
2022-09-17 13:55:59 +0300

    optimization

---
+++ src/components/app/App.js
@@ -0,0 +1,31 @@
+
+import AppHeader from "../appHeader/AppHeader";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import  {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
+
+
+
+
+const App = () => {
+   
+
+    return (
+       <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}></Route>
+                        <Route path="/comics" element={<ComicsPage/>}></Route>
+                        <Route path="/comics/:comicId " element={<SingleComicPage/>}></Route>
+                        <Route path="*" element={<Page404/>}></Route>
+
+                    </Routes>
+                </main>
+            </div>
+       </Router>
+    )
+}
+
+export default App;
+
